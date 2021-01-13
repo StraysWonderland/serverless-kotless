ed97f023267eace2c066ddcd4b97a27c8211e61b
Cagri Tasci <cagri.c.tasci@outlook.de>
2021-01-13 20:34:15 +0100

    breakevencalculation added

--- break-even-ktor/src/Application.kt
+++ break-even-ktor/src/Application.kt
@@ -6,6 +6,7 @@ import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
 import software.amazon.ion.system.IonTextWriterBuilder.json
+import kotlin.math.ceil
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -13,9 +14,17 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(ContentNegotiation) {
+        json()
+    }
+
     routing {
-        get("/") {
-            call.respondText("Some Test")
+        post("/") {
+            val breakEvenRequest = call.receive<BreakEvenRequest>()
+            val breakEvenPoint =
+                ceil(breakEvenRequest.fixedCosts / (breakEvenRequest.price - breakEvenRequest.unitCosts)).toInt()
+            call.respond(breakEvenPoint)
         }
     }
 }
