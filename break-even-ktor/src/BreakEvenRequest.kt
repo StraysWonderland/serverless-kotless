7e5f4067513c082a3508c254fee0cb7b3bf0997f
Cagri Tasci <cagri.c.tasci@outlook.de>
2021-01-13 22:50:48 +0100

    Update BreakEvenRequest.kt

--- break-even-ktor/src/BreakEvenRequest.kt
+++ break-even-ktor/src/BreakEvenRequest.kt
@@ -2,4 +2,4 @@ package info.novatec
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class BreakEvenRequest(val fixedCosts: Double, val unitCosts: Double, val price: Double)
+data class BreakEvenRequest(val price: Double, val fixedCosts: Double, val unitCosts: Double)
