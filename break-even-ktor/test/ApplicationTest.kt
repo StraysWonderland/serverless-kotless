67a39c87fa90d5e7f5ab8bf4be7f54fa455b6504
Cagri Tasci <cagri.c.tasci@outlook.de>
2021-02-05 12:48:17 +0100

    test cleaned

--- break-even-ktor/test/ApplicationTest.kt
+++ break-even-ktor/test/ApplicationTest.kt
@@ -1,21 +1,17 @@
 package info.novatec.serverless
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
-import io.ktor.features.*
-import kotlin.test.*
 import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 class ApplicationTest {
     @Test
     fun testRequests() = withTestApplication(Application::module) {
         val call = handleRequest(HttpMethod.Post, "/") {
-            addHeader(HttpHeaders.Accept,ContentType.Application.Json.toString())
+            addHeader(HttpHeaders.Accept, ContentType.Application.Json.toString())
             addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
             setBody(
                 """{
@@ -28,9 +24,6 @@ class ApplicationTest {
         with(call) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertEquals("10", response.content)
-        }
-        with(handleRequest(HttpMethod.Get, "/index.html")) {
-            assertFalse(requestHandled)
         }
     }
 }
