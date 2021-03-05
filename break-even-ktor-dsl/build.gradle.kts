91da18019e5913f81f538f930e3f2888aae6d7c6
Cagri Tasci <cagri.c.tasci@outlook.de>
2021-03-05 12:08:56 +0100

    changed break-even-ktor-dsl to function with kotless server class only

--- break-even-ktor-dsl/build.gradle.kts
+++ break-even-ktor-dsl/build.gradle.kts
@@ -1,20 +1,16 @@
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.kotless.plugin.gradle.dsl.kotless
 
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-
-plugins {
-    kotlin("jvm") version "1.3.72" apply true
-    id("io.kotless") version "0.1.7-beta-4" apply true
-}
+val kotless_version: String by project
 
 group = "info.novatec"
 version = "0.0.1"
 
-application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
+plugins {
+    kotlin("jvm") version "1.3.72" apply true
+    id("io.kotless") version "0.1.7-beta-4" apply true
 }
 
 repositories {
@@ -26,8 +22,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    implementation("io.kotless", "ktor-lang", "0.1.7-beta-4")
-    implementation ("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.kotless", "ktor-lang", "$kotless_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
@@ -35,3 +31,22 @@ kotlin.sourceSets["test"].kotlin.srcDirs("test")
 
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
+
+
+kotless {
+    config {
+        bucket = "my.kotless.bucket"
+
+        terraform {
+            profile = "my.kotless.user"
+            region = "eu-west-1"
+        }
+    }
+    webapp {
+        lambda {
+            kotless {
+                packages = setOf("info.novatec")
+            }
+        }
+    }
+}
--- break-even-ktor-dsl/src/Server.kt
+++ break-even-ktor-dsl/src/Server.kt
@@ -2,23 +2,31 @@ package info.novatec
 
 import io.kotless.dsl.ktor.Kotless
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import kotlinx.coroutines.Dispatchers
 import kotlin.math.ceil
 
 class Server : Kotless() {
     override fun prepare(app: Application) {
+        app.install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+                serializeNulls()
+            }
+        }
         app.routing {
             get("/") {
                 call.respondText { "Hello World!" }
             }
             post("/") {
-                    val breakEvenRequest = call.receive<BreakEvenRequest>()
-                    val breakEvenPoint =
-                        ceil(breakEvenRequest.fixedCosts / (breakEvenRequest.price - breakEvenRequest.unitCosts)).toInt()
-                    call.respond(breakEvenPoint)
+
+                val breakEvenRequest = call.receive<BreakEvenRequest>()
+                val breakEvenPoint =
+                    ceil(breakEvenRequest.fixedCosts / (breakEvenRequest.price - breakEvenRequest.unitCosts)).toInt()
+                call.respond(breakEvenPoint)
             }
         }
     }
